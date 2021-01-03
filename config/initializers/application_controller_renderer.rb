678799e4db071c01ebba23fdeac4830b821861e7
Junsasaki0077 <ikasas0077@gmail.com>
2021-01-03 15:55:23 +0900

    user updateアクションの追加 ヘッダーにユーザー一覧ボタン表示

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'efb358e18d132f4632e276dca15ad6413506e87c30c9dffbb9daead5b29e38dfbd4e17fac9288874890ac70e6d718a6b8967646716476a7d331c0b8961f558f9'
